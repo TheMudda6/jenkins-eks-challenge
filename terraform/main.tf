a40938b9ed6e113dd1a8a71c8ea51be92f680a19
TheMudda6 <Mudassirshaikh1997@outlook.com>
2026-06-03 10:53:15 +0100

    Added authentication

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,7 +36,7 @@ module "eks" {
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
-  
+  enable_cluster_creator_admin_permissions = true  
 
   vpc_id = module.vpc.vpc_id
 
