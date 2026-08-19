2fac8e3240146284f28a53df935c35e467bb3d72
TheMudda6 <Mudassirshaikh1997@outlook.com>
2026-08-19 14:13:31 +0100

    feat: bootstrap ArgoCD GitOps

--- terraform/modules/argocd/main.tf
+++ terraform/modules/argocd/main.tf
@@ -7,4 +7,43 @@ resource "helm_release" "argocd" {
   create_namespace = true
 
   version = var.chart_version
+
+  values = [
+    yamlencode({
+      extraObjects = [
+        {
+          apiVersion = "argoproj.io/v1alpha1"
+          kind       = "Application"
+
+          metadata = {
+            name      = "root"
+            namespace = var.namespace
+          }
+
+          spec = {
+            project = "default"
+
+            source = {
+              repoURL        = "https://github.com/TheMudda6/jenkins-eks-challenge.git"
+              targetRevision = "main"
+              path           = "kubernetes/applications"
+            }
+
+            destination = {
+              server    = "https://kubernetes.default.svc"
+              namespace = var.namespace
+            }
+
+            syncPolicy = {
+              automated = {
+                prune    = true
+                selfHeal = true
+              }
+            }
+          }
+        }
+      ]
+    })
+  ]
+
 }
